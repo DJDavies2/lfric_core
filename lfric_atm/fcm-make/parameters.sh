af3196e3d9dd2e8977a32c3376b57790f8db5138
matthewhambley <enquiries@metoffice.gov.uk>
2020-05-20 07:03:43 +0000

    ticket:2207 for Ian Boutle: Upward SW flux on tiles is missing.
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@23732 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- lfric_atm/fcm-make/parameters.sh
+++ lfric_atm/fcm-make/parameters.sh
@@ -7,7 +7,9 @@ export jules_rev=um11.6
 export jules_sources=
 export keys_atmos_extra=LFRIC=lfric
 export socrates_rev=um11.6
-export socrates_sources=
+export socrates_sources='fcm:socrates.xm_br/dev/jamesmanners/um11.6_tile_all@897'
 export steplist="extract preprocess-atmos"
 export um_rev=vn11.6
 export um_sources=
+
+#88: um11.6_tile_all targetted at 11.7
