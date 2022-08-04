031f64226298caa4a6d2b9f2ad2411a8afd2c65e
ivakavcic <enquiries@metoffice.gov.uk>
2022-08-04 08:21:53 +0000

    ticket:3401 for Mike Hobson: Global reductions in different precisions
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@37384 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

---
+++ infrastructure/source/scalar/r_tran_scalar_mod.F90
@@ -0,0 +1,29 @@
+!-----------------------------------------------------------------------------
+! (C) Crown copyright 2022 Met Office. All rights reserved.
+! The file LICENCE, distributed with this code, contains details of the terms
+! under which the code may be used.
+!-----------------------------------------------------------------------------
+!
+!> @brief A module providing r_tran scalar related classes.
+!>
+!> @details This is a version of a scalar object that can hold an r_tran data
+!> value.
+
+module r_tran_scalar_mod
+
+  ! Eventually the precision of scalar data for use in transport will be set
+  ! in a module held within the model (as it is model information). For now,
+  ! PSyclone is expecting to "use" the definitions from here, so this is where
+  ! it is set
+#if (R_TRAN_PRECISION == 32)
+  use scalar_r32_mod, only: r_tran_scalar_type => scalar_r32_type
+#else
+  use scalar_r64_mod, only: r_tran_scalar_type => scalar_r64_type
+#endif
+
+  implicit none
+  private
+
+  public :: r_tran_scalar_type
+
+end module r_tran_scalar_mod
